dd230a63c0bc2f9e784d452af7d83d7f863c95f6
Tomoki <tomoki_ota@outlook.com>
2022-11-08 00:01:09 +0900

    completed header and hero section for mobile

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const UIHamburger = document.querySelector(".mobile-hamburger");
+const UIMobileNav = document.querySelector(".mobile-nav-links");
+let menuIsOpen = false;
+
+UIHamburger.addEventListener("click", openMenu);
+
+function openMenu() {
+    if (!menuIsOpen) {
+        UIMobileNav.style.transform = "translateY(0)";
+        UIMobileNav.style.opacity = 1;
+        UIMobileNav.style.zIndex = "10";
+        UIHamburger.style.backgroundColor = "#7e5ae2";
+    } else {
+        UIMobileNav.style.transform = "translateY(-25px)";
+        UIMobileNav.style.opacity = 0;
+        UIMobileNav.style.zIndex = "-10";
+        UIHamburger.style.backgroundColor = "#050505";
+    }
+
+    menuIsOpen = !menuIsOpen;
+}
